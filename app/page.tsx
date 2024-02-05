ca054a9e4313316cb29dca1a96194af60aac78f5
Ray Kuo <enihu@gmail.com>
2024-02-05 16:46:38 +1300

    Added delete shapes feature

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import { useEffect, useRef, useState } from "react";
 import { handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleCanvaseMouseMove, handleResize, initializeFabric, renderCanvas } from "@/lib/canvas";
 import { ActiveElement } from "@/types/type";
 import { useMutation, useStorage } from "@/liveblocks.config";
+import { defaultNavElement } from "@/constants";
+import { handleDelete } from "@/lib/key-events";
 
 export default function Page() {
 
@@ -42,8 +44,45 @@ export default function Page() {
     icon: '',
   });
 
+  const deleteAllShapes = useMutation(({ storage }) => {
+    const canvasObjects = storage.get('canvasObjects');
+
+    if (!canvasObjects || canvasObjects.size === 0) return true;
+
+    for (const [key, value] of canvasObjects.entries()) {
+      canvasObjects.delete(key);
+    }
+
+    return canvasObjects.size === 0;
+  }, []);
+
+  const deleteShapeFromStorage = useMutation(({ storage }, objectId) => {
+    const canvasObjects = storage.get('canvasObjects');
+    canvasObjects.delete(objectId);
+  }, []);
+
   const handleActiveElement = (elem : ActiveElement) => {
     setActiveElement(elem);
+
+    switch (elem?.value) {
+      case 'reset':
+        deleteAllShapes();
+        fabricRef.current?.clear();
+        setActiveElement(defaultNavElement);
+        break;
+
+      case 'delete':
+        handleDelete(
+          fabricRef.current as any,
+          deleteShapeFromStorage,
+        );
+        setActiveElement(defaultNavElement);
+        break;
+
+      default:
+        break;
+    }
+
     selectedShapeRef.current = elem?.value as string;
   }
 
@@ -91,7 +130,11 @@ export default function Page() {
 
     window.addEventListener('resize', () => {
       handleResize({ canvas: fabricRef });
-    })
+    });
+
+    return () => {
+      canvas.dispose();
+    }
   }, []);
 
   useEffect(() => {
