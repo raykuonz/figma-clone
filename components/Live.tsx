ff33132974d477f141348c04ae1d0c262a5a7507
Ray Kuo <enihu@gmail.com>
2024-02-08 13:32:46 +1300

    Fixed types and disable SSR

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,8 +1,8 @@
-import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from "@/liveblocks.config"
+import { useBroadcastEvent, useEventListener, useMyPresence } from "@/liveblocks.config"
 import LiveCursors from "./cursor/LiveCursors"
 import React, { useCallback, useEffect, useState } from "react";
 import CursorChat from "./cursor/CursorChat";
-import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import { CursorMode, CursorState, Reaction } from "@/types/type";
 import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
@@ -29,8 +29,7 @@ const Live = ({
   redo,
 }: Props) => {
 
-  const others = useOthers();
-  const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [{ cursor }, updateMyPresence] = useMyPresence();
 
   const [cursorState, setCursorState] = useState<CursorState>({
     mode: CursorMode.Hidden,
@@ -67,7 +66,7 @@ const Live = ({
   }, 100);
 
   useEventListener((eventData) => {
-    const event = eventData.event as ReactionEvent;
+    const event = eventData.event;
 
     setReaction((reactions) => reactions.concat([{
       point: { x: event.x, y: event.y },
@@ -229,7 +228,7 @@ const Live = ({
           />
         ))}
 
-        <LiveCursors others={others} />
+        <LiveCursors />
 
         <Comments />
       </ContextMenuTrigger>
